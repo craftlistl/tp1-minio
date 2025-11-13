0f76e0cdfbbc4a70d2954cfc388ff9a95d0e7940
craftlistl <adrienlistl@gmail.com>
2025-11-13 08:37:24 +0100

    init

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        minio = {
+            source = "terraform-provider-minio/minio"
+            version = ">= 3.1.0"
+        }
+    }
+}
+
+provider "minio" {
+    endpoint = "http://localhost:9000"
+    minio_access_key = "minioadmin"
+    minio_secret_key = "minioadmin"
+}
+resource "minio_s3_bucket" "tp1_bucket" {
+    bucket = "tp1-cloud-bucket"
+    acl = "private"
+}
