ac3e28c27cff81630e67a4de173061350e4d62f4
craftlistl <adrienlistl@gmail.com>
2025-11-14 09:30:25 +0100

    feat(acl): access only for index and css

--- main.tf
+++ main.tf
@@ -16,13 +16,20 @@ provider "minio" {
 
 resource "minio_s3_bucket" "tp1_bucket" {
   bucket = "tp1-cloud-bucket"
-  acl    = "public-read"
+  acl    = "private"
 }
 
 resource "minio_s3_object" "index_html" {
   bucket_name  = minio_s3_bucket.tp1_bucket.bucket
   object_name  = "index.html"
   source       = "index.html"
+  content_type = "text/html"
+}
+
+resource "minio_s3_object" "blocked_html" {
+  bucket_name  = minio_s3_bucket.tp1_bucket.bucket
+  object_name  = "blocked.html"
+  source       = "blocked.html"
   content_type = "text/html"
 }
 
@@ -33,8 +40,9 @@ resource "minio_s3_object" "style_css" {
   content_type = "text/css"
 }
 
-resource "minio_iam_policy" "public_policy" {
-  name = "public-policy"
+resource "minio_s3_bucket_policy" "public_policy" {
+  bucket = minio_s3_bucket.tp1_bucket.bucket
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -43,7 +51,10 @@ resource "minio_iam_policy" "public_policy" {
       "Effect": "Allow",
       "Principal": "*",
       "Action": ["s3:GetObject"],
-      "Resource": "arn:aws:s3:::tp1-cloud-bucket/*"
+      "Resource": [
+        "arn:aws:s3:::tp1-cloud-bucket/index.html",
+        "arn:aws:s3:::tp1-cloud-bucket/style.css"
+      ]
     }
   ]
 }
